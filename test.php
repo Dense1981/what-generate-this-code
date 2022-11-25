97cc0202a0d592dee914cf59a0eabadc1687dda5
Dense <73557345+Dense1981@users.noreply.github.com>
2022-11-25 23:56:47 +0100

    Update test.php

--- test.php
+++ test.php
@@ -9,11 +9,11 @@
  $c = $a = $b;
 foreach(array($c) as $d){
  $x = strpos($c, $c[0]);
- $d = "\$_REQUEST[$d]";
+ $d = "/\$_REQUEST[$d]/";
 
 for(; $x < strlen($d); $x++) $e.= $d[$x];
 }
 
-echo str_replace(array('$_REQUEST[',']', '// '), '', $e);
+echo str_replace(array('$_REQUEST[',']', '/'), '', $e);
 
 ?>
