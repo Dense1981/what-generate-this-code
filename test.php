2138913ebb5d666c0434279c7a3b2784c8887d33
Dense <73557345+Dense1981@users.noreply.github.com>
2022-11-25 18:57:20 +0100

    Create test.php
    What do you think is being spent here? The solution is surprising!
    
    Was denkt ihr wird hier ausgegeben? Die Lösung ist überraschend!

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+ $a = basename(__FILE__);
+ $b = '
+
+// Was wird ausgegeben?
+
+';
+ $c = $a = $b;
+foreach(array($c) as $d){
+ $x = strpos($c, $c[0]);
+ $d = "\$_REQUEST[$d]";
+
+for(; $x < strlen($d); $x++) $e.= $d[$x];
+}
+
+echo str_replace(array('$_REQUEST[',']', '// '), '', $e);
+
+?>
